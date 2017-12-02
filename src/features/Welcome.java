d3f6091cfeaf49d9df35b805e3b926089176901a
exploitedwe <34082024+exploitedwe@users.noreply.github.com>
2017-12-01 20:23:21 -0500

    fix24

--- src/features/Welcome.java
+++ src/features/Welcome.java
@@ -1,13 +1,32 @@
-package BotProject.features;
+package features;
 
-import BotProject.DiscordBot.Feature;
-import net.dv8tion.jda.core.JDA;
+import DiscordBot.DevBot;
+import net.dv8tion.jda.core.EmbedBuilder;
+import net.dv8tion.jda.core.entities.Guild;
+import net.dv8tion.jda.core.entities.Member;
+import net.dv8tion.jda.core.events.guild.voice.GuildVoiceJoinEvent;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 
-public class Welcome implements Feature {
+import java.awt.*;
+import java.util.List;
 
-    @Override
-    public void action(String[] args, MessageReceivedEvent event) {
+public class Welcome {
 
+    public static void handleFeature(GuildVoiceJoinEvent event, int role) {
+        switch (role) {
+            case 1: // Regular
+                    event.getGuild().getTextChannelById(DevBot.textID())
+                            .sendMessage("Welcome " + event.getMember().getEffectiveName() + " :wave:").complete();
+                break;
+            case 2: // God
+                    event.getGuild().getTextChannelById(DevBot.textID())
+                            .sendMessage(new EmbedBuilder().setColor(Color.YELLOW)
+                                    .setDescription("Gaming God " + event.getMember().getEffectiveName() +
+                                    " has graced us with his presence.").setTitle("BOW DOWN PLEBS")
+                                    .setThumbnail("https://ih0.redbubble.net/image.363620937.6389/flat,800x800,075,t.u2.jpg")
+                                    .build())
+                            .complete();
+                break;
+        }
     }
 }
